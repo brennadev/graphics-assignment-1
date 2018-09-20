5797aa3190a286c2ac15829eea02243a5163aa15
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2018-09-20 07:42:00 -0500

    Initial Commit

---
+++ Assignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Brenna Olson on 9/20/18.
+//  Copyright © 2018 Brenna Olson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
