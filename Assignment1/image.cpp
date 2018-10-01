22140fda5f1cd6ad05164b7ca05801eaede2eb65
Brenna Olson <36449761+brennadev@users.noreply.github.com>
2018-10-01 10:07:59 -0500

    Edge detect first attempt - doesn't appear to totally work

--- Assignment1/image.cpp
+++ Assignment1/image.cpp
@@ -458,12 +458,21 @@ void Image::Sharpen(int n) {
 
 void Image::EdgeDetect() {
     
+    // when doing the convolution math, we always need to pull from the original image, not the partially edge detected version of the original image
+    Image *originalImage = new Image(*this);
+    
+    
+    
+    const int n = 3;
+    
     // TODO: put the convolution code below the filter declaration once it works in the blur code
     // TODO: put a call to blur after detecting edges to smooth it out (once blur works)
     // the filter for edge detect is always the same
-    float filter[3][3] = {{ -1, -1, -1 },
+    float filter[n][n] = {{ -1, -1, -1 },
                           { -1, 8, -1 },
                           { -1, -1, -1 }};
+    
+    int filterTotalNumberOfElements = n * n;
     
     // can probably just remove this as a bunch of convolution code will just be pasted here (or maybe just put the convolution code in a separate private method that's called by blur and edge detect)
     for (int i = 0; i < width; i++) {
@@ -471,6 +480,85 @@ void Image::EdgeDetect() {
             
         }
     }
+    
+    
+    // actual convolution - this section is identical to the blur code
+    // go through each location in the image
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            
+            // location of intermediate values after multiplication
+            Pixel currentlyMultipliedByFilter[n][n];
+            
+            // so the correct location in the image is multiplied by the corresponding filter location - increment by 1 as necessary
+            int currentImageLocationForFilterX = -1 * n / 2;
+            int currentImageLocationForFilterY = -1 * n / 2;
+            
+            // go through each location in the filter and multiply
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < n; l++) {
+                    
+                    // take care of the edges by extending the pixels closest to the edges
+                    // start with default locations
+                    int xLocationInImageToMultiply = i + currentImageLocationForFilterX;
+                    int yLocationInImageToMultiply = j + currentImageLocationForFilterY;
+                    
+                    // if x goes off the left edge
+                    if (i + currentImageLocationForFilterX < 0) {
+                        xLocationInImageToMultiply = 0;
+                        
+                        // if x goes off the right edge
+                    } else if (i + currentImageLocationForFilterX >= width) {
+                        xLocationInImageToMultiply = width - 1;
+                    }
+                    
+                    // if y goes off the top edge
+                    if (j + currentImageLocationForFilterY < 0) {
+                        yLocationInImageToMultiply = 0;
+                        
+                        // if y goes off the bottom edge
+                    } else if (j + currentImageLocationForFilterY >= height) {
+                        yLocationInImageToMultiply = height - 1;
+                    }
+                    
+                    // the corrected locations get passed to the multiplication so it always will work
+                    currentlyMultipliedByFilter[k][l] = originalImage->GetPixel(xLocationInImageToMultiply, yLocationInImageToMultiply) * filter[k][l];
+                    
+                    currentImageLocationForFilterY++;
+                }
+                currentImageLocationForFilterX++;
+            }
+            
+            // due to clamping, averaging the components needs to be done by channel (ignoring alpha)
+            int redTotal = 0;
+            int greenTotal = 0;
+            int blueTotal = 0;
+            
+            for (int k = 0; k < n; k++) {
+                for (int l = 0; l < n; l++) {
+                    redTotal += currentlyMultipliedByFilter[k][l].r;
+                    greenTotal += currentlyMultipliedByFilter[k][l].g;
+                    blueTotal += currentlyMultipliedByFilter[k][l].b;
+                }
+            }
+            
+            
+            // actually take the averages - no clamping should be needed as all the original values are between 0 and 255
+            redTotal /= filterTotalNumberOfElements;
+            greenTotal /= filterTotalNumberOfElements;
+            blueTotal /= filterTotalNumberOfElements;
+            
+            // finally, put it back in the original
+            GetPixel(i, j).r = redTotal;
+            GetPixel(i, j).g = greenTotal;
+            GetPixel(i, j).b = blueTotal;
+        }
+    }
+    
+    
+    
+    
+    
 }
 
 // TODO: test this function; it should be complete, but Sample isn't implemented yet, so I can't test it
